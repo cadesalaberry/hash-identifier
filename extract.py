e90d9a8fd7dea06e478213eaa686e3a7818508f7
C-A de Salaberry <cadesalaberry@yahoo.com>
2014-05-29 00:34:19 -0400

    Add: initial code.

---
+++ extract.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+"""
+encoding: utf-8
+Hash Identifier v1.2
+By C4Labz (From Zion3R)
+www.c4labz.com
+"""
+
+import re
+import json
+
+dic = [{}]
+algorithms = {}
+
+def get_name(l):
+	return re.match('def (\w+)\(\)\:', l).group(1)
+
+def get_sample(l):
+	return re.match('.+hs=\'(.+)\'', l).group(1)
+
+def get_id(l):
+	return re.match('.+jerar.append\(\"(.+)\"\)', l).group(1)
+
+def get_format_rules(l):
+	return re.match('.+(if len\(hash\)==len\(hs\)).+', l)
+
+
+def scan(line):
+
+	try:
+		n = get_name(line)
+		dic.append({'n':n})
+
+	except AttributeError:
+		pass
+
+	try:
+		s = get_sample(line)
+		dic[-1]['s'] = s
+
+	except AttributeError:
+		pass
+
+	try:
+		r = get_format_rules(line)
+		dic[-1]['r'] = r
+
+	except AttributeError:
+		pass
+
+	try:
+	 	i = get_id(line)
+	 	dic[-1]['i'] = i
+	
+	except AttributeError:
+	 	pass
+
+def main():
+
+	
+
+	with open('Hash_ID_v1.1.py') as f:
+		content = f.readlines()
+
+		for line in content:
+			scan(line)
+
+	print json.dumps(dic, sort_keys=True, indent=4)
+
+
+
+if __name__ == '__main__':
+	main()
