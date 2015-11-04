fb483cb7371e17d47f181e6c56ee60359be91d96
C-A de Salaberry <cadesalaberry@yahoo.com>
2015-11-04 17:10:12 +0100

    Add: Starts with rule.

--- extract.py
+++ extract.py
@@ -9,77 +9,121 @@ www.c4labz.com
 import re
 import json
 
-dic = [{}]
+dic = []
 algorithms = {}
 
 def get_name(l):
-	# Extracts "FCS16" from "def FCS16():"
-	return re.match('def (\w+)\(\)\:', l).group(1)
+  # Extracts "FCS16" from "def FCS16():"
+  return re.match('def (\w+)\(\)\:', l).group(1)
 
 def get_sample(l):
-	# Extracts "4607" from "hs='4607'"
-	return re.match('.+hs=\'(.+)\'', l).group(1)
+  # Extracts "4607" from "hs='4607'"
+  return re.match('.+hs=\'(.+)\'', l).group(1)
 
 def get_id(l):
-	# Extracts "101020" from "jerar.append("101020")"
-	return re.match('.+jerar.append\(\"(.+)\"\)', l).group(1)
-
-def get_format_rules(l):
-	re.match('.+(if len\(hash\)==len\(hs\)).+', l).group(1)
-	rules = {}
-
-	if re.match('hash.isdigit()==False', l):
-		rules['d'] = 0
-		print "yo"
-	elif re.match('hash.isdigit()==True', l):
-		rules['d'] = 1
-
-	return rules
+  # Extracts "101020" from "jerar.append("101020")"
+  return re.match('.+jerar.append\(\"(.+)\"\)', l).group(1)
 
 
-def scan(line):
+def extract_boolean(variable, line):
 
-	try:
-		n = get_name(line)
-		dic.append({'n':n})
+  if variable + '==True' in line:
+    return True
+  elif variable + '==False' in line:
+    return False
+  else:
+    raise ValueError('Confusing boolean for ' + variable)
 
-	except AttributeError:
-		pass
 
-	try:
-		s = get_sample(line)
-		dic[-1]['s'] = s
 
-	except AttributeError:
-		pass
+def get_format_rules(line):
+  re.match('.+(if len\(hash\)==len\(hs\)).+', line).group(1)
 
-	try:
-		r = get_format_rules(line)
-		dic[-1]['r'] = r
+  format_rules = {}
 
-	except AttributeError:
-		pass
+  booleans_to_scan = [
+    ('hash.isdigit()', 'digit'),
+    ('hash.isalpha()', 'alpha'),
+    ('hash.isalnum()', 'alnum')
+  ]
 
-	try:
-	 	i = get_id(line)
-	 	dic[-1]['i'] = i
-	
-	except AttributeError:
-	 	pass
+  regex_to_scan = [
+    ('.+hash\[0:\d+\]\.find\(\'(.+)\'\)==0.+', 'starts_with')
+  ]
+
+  # Extracts boolean rules
+  for variable, name in booleans_to_scan:
+    try:
+      if variable in line:
+        v = extract_boolean(variable, line)
+        print variable, ":", v
+        format_rules.update({name: v})
+
+    except ValueError:
+      pass
+
+  # Extracts regex rules
+  for regex_rule, name in regex_to_scan:
+    try:
+      v = re.match(regex_rule, line).group(1)
+      if v:
+        print name, ":", variable, ":", v
+        format_rules.update({name: v})
+
+    except AttributeError:
+      pass
+
+  return format_rules
+
+
+def scan(line, i=-1):
+
+  try:
+    n = get_name(line)
+    dic.append({'name': n})
+    print i, "\tName:", n
+
+  except AttributeError:
+    pass
+
+  try:
+    s = get_sample(line)
+    dic[-1]['sample'] = s
+    print i, "\t\tSample:", s
+
+    dic[-1]['size'] = len(s)
+    print i, "\t\tSize:", len(s)
+
+  except AttributeError:
+    pass
+
+  try:
+    r = get_format_rules(line)
+    dic[-1]['rules'] = r
+    print i, "\t\tRules:", len(r)
+
+  except AttributeError:
+    pass
+
+  try:
+    hash_id = get_id(line)
+    dic[-1]['id'] = hash_id
+    print i, "\t\tHash ID:", hash_id
+
+  except AttributeError:
+    pass
 
 def main():
 
-	
+  with open('sample') as f:
+    content = f.readlines()
 
-	with open('sample') as f:
-		content = f.readlines()
+    for i, line in enumerate(content):
+      scan(line, i)
 
-		for line in content:
-			scan(line)
-
-	print json.dumps(dic, sort_keys=True, indent=2)
+  print json.dumps(dic, sort_keys=True, indent=2)
 
 
 
 if __name__ == '__main__':
-	main()
+  main()
