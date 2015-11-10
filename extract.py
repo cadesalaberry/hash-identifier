02d8e1f499072865b7a889bcbed63f89c25d32f0
C-A de Salaberry <cadesalaberry@yahoo.com>
2015-11-10 17:03:17 +0100

    Add: Disable logging. Output json as files.

--- extract.py
+++ extract.py
@@ -12,19 +12,20 @@ import json
 dic = []
 algorithms = {}
 
+# Extracts "FCS16" from "def FCS16():"
 def get_name(l):
-  # Extracts "FCS16" from "def FCS16():"
   return re.match('def (\w+)\(\)\:', l).group(1)
 
+# Extracts "4607" from "hs='4607'"
 def get_sample(l):
-  # Extracts "4607" from "hs='4607'"
   return re.match('.+hs=\'(.+)\'', l).group(1)
 
+# Extracts "101020" from "jerar.append("101020")"
 def get_id(l):
-  # Extracts "101020" from "jerar.append("101020")"
   return re.match('.+jerar.append\(\"(.+)\"\)', l).group(1)
 
-
+# Returns a boolean from a line like :
+# "... and hash.isdigit()==False and ..."
 def extract_boolean(variable, line):
 
   if variable + '==True' in line:
@@ -35,43 +36,74 @@ def extract_boolean(variable, line):
     raise ValueError('Confusing boolean for ' + variable)
 
 
+# Extracts boolean rules from a list like :
+# booleans_to_scan = [('hash.isdigit()', 'digits_only')]
+def extract_booleans(line, booleans_to_scan):
+
+  rules = {}
+
+  for variable, name in booleans_to_scan:
+    try:
+      if variable in line:
+
+        v = extract_boolean(variable, line)
+
+        rules[name] = v
+
+    except ValueError:
+      pass
+
+  return rules
+
+
+def extract_regexes(line, regexes_to_scan):
+
+  rules = {}
+
+  # Extracts regex rules
+  for regex_rule, name in regexes_to_scan:
+    try:
+      v = re.match(regex_rule, line).group(1)
+      if v:
+        # Turns the value into an int if possible
+        try:
+          v = int(v)
+        except ValueError:
+          pass
+
+        rules[name] = v
+
+    except AttributeError:
+      pass
+
+  return rules
+
 
 def get_format_rules(line):
+
+  # Returns an AttributeError if not a format_rule
   re.match('.+(if len\(hash\)==len\(hs\)).+', line).group(1)
 
   format_rules = {}
 
   booleans_to_scan = [
-    ('hash.isdigit()', 'digit'),
-    ('hash.isalpha()', 'alpha'),
-    ('hash.isalnum()', 'alnum')
+    ('hash.isdigit()', 'digits_only'),
+    ('hash.isalpha()', 'letters_only'),
+    ('hash.isalnum()', 'alnumeric'),
+    ('hash.islower()', 'lowercase')
   ]
 
   regex_to_scan = [
-    ('.+hash\[0:\d+\]\.find\(\'(.+)\'\)==0.+', 'starts_with')
+    ('.+hash\[0:\d+\]\.find\(\'(.+)\'\)==0.+', 'starts_with'),
+    ('.+hash\[(\d+):\d+\]\.find\(\':\'\)==0.+', 'colon_index')
   ]
 
-  # Extracts boolean rules
-  for variable, name in booleans_to_scan:
-    try:
-      if variable in line:
-        v = extract_boolean(variable, line)
-        print variable, ":", v
-        format_rules.update({name: v})
+  booleans = extract_booleans(line, booleans_to_scan)
+  format_rules.update(booleans)
 
-    except ValueError:
-      pass
 
-  # Extracts regex rules
-  for regex_rule, name in regex_to_scan:
-    try:
-      v = re.match(regex_rule, line).group(1)
-      if v:
-        print name, ":", variable, ":", v
-        format_rules.update({name: v})
-
-    except AttributeError:
-      pass
+  regexes = extract_regexes(line, regex_to_scan)
+  format_rules.update(regexes)
 
   return format_rules
 
@@ -81,7 +113,6 @@ def scan(line, i=-1):
   try:
     n = get_name(line)
     dic.append({'name': n})
-    print i, "\tName:", n
 
   except AttributeError:
     pass
@@ -89,10 +120,7 @@ def scan(line, i=-1):
   try:
     s = get_sample(line)
     dic[-1]['sample'] = s
-    print i, "\t\tSample:", s
-
     dic[-1]['size'] = len(s)
-    print i, "\t\tSize:", len(s)
 
   except AttributeError:
     pass
@@ -100,7 +128,6 @@ def scan(line, i=-1):
   try:
     r = get_format_rules(line)
     dic[-1]['rules'] = r
-    print i, "\t\tRules:", len(r)
 
   except AttributeError:
     pass
@@ -108,22 +135,28 @@ def scan(line, i=-1):
   try:
     hash_id = get_id(line)
     dic[-1]['id'] = hash_id
-    print i, "\t\tHash ID:", hash_id
 
   except AttributeError:
     pass
 
 def main():
 
-  with open('sample') as f:
+  # with open('sample') as f:
+  with open('Hash_ID_v1.1.py') as f:
     content = f.readlines()
 
     for i, line in enumerate(content):
       scan(line, i)
 
-  print json.dumps(dic, sort_keys=True, indent=2)
+  readable_json = json.dumps(dic, sort_keys=True, indent=2)
 
+  print readable_json
 
+  with open('hash-identifier.db.json', 'w') as o:
+    o.write(readable_json)
+
+  with open('hash-identifier.db.min.json', 'w') as o:
+    json.dump(dic, o)
 
 if __name__ == '__main__':
   main()
